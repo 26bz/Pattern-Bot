66e16b7a3f111efd111b955ebf9d327db24c628a
26bz <109409095+26bz@users.noreply.github.com>
2025-07-21 21:13:12 -0400

    refactor: consolidate event handling logic

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-import { client } from '../index.js';
-import { logger } from '../logger.js';
-
-client.once('ready', () => {
-  logger.info(`Logged in as ${client.user.tag}`);
-  logger.info('Bot is ready to respond to questions!');
-});
